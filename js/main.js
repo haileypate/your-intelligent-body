448c765545ec3932fcafa066ed334ed24010ca3e
Hailey Pate <2haileypate@gmail.com>
2020-03-16 00:50:36 -0500

    shameful catch up

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+// when button#show-menu is clicked, 
+  // show the #top-menu 
+  // change the button#show-menu label to 'X' for close menu
+
+
+// when button#show-menu is clicked again, 
+   // hide the #top-menu 
+   // change the button label back to 'MENU'
+   // bring betsy img to position
+
+// when button is clicked and user scrolls down far enough that #top-menu is no longer visible to viewport, 
+   // hide the #top-menu 
+   // change the button label back to 'MENU'
+   // bring betsy img to position
+
+// when any #top-menu element is clicked
+  // hide the #top-menu 
+   // change the button label back to 'MENU'
